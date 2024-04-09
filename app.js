1319b5a8b44622db8cd213145c6eca74a31df3a2
Suneha <ssuneha21@gmail.com>
2024-04-09 21:01:22 +0530

    commit

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const mysql = require('mysql')
+const CryptoJS = require('crypto-js')
+
+const key = ")(*&^%$#@!1234567890asdfghjklmnbvcxzqwertyuiop"
+
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
+
+const io = require('socket.io')(server)
+ 
+//database connection
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'mysql123',
+  database: 'chatapp'
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket)
+{
+  console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+
+  socket.on('message', (data) => {
+
+/*decrypting ciphertext using DES
+
+    let start = Date.now()
+    const decrypted = CryptoJS.DES.decrypt(data.message, key, {
+      mode: CryptoJS.mode.CTR,
+      padding: CryptoJS.pad.Pkcs7,
+    }).toString(CryptoJS.enc.Utf8);
+    let end = Date.now()
+*/
+
+/*decrypting ciphertext using TripleDES
+
+    let start = Date.now()
+    const decrypted = CryptoJS.TripleDES.decrypt(data.message, key, {
+      mode: CryptoJS.mode.CTR,
+      padding: CryptoJS.pad.Pkcs7,
+    }).toString(CryptoJS.enc.Utf8);
+    let end = Date.now()
+*/
+
+/*decrypting ciphertext using RC4
+    
+    let start = Date.now()
+    const decrypted = CryptoJS.RC4.decrypt(data.message, key, {
+      mode: CryptoJS.mode.CTR,
+      padding: CryptoJS.pad.Pkcs7,
+    }).toString(CryptoJS.enc.Utf8);
+    let end = Date.now()
+*/
+
+/*decrypting ciphertext using AES
+*/
+    let start = Date.now()
+    const decrypted = CryptoJS.AES.decrypt(data.message, key, {
+      mode: CryptoJS.mode.CTR,
+      padding: CryptoJS.pad.Pkcs7,
+    }).toString(CryptoJS.enc.Utf8);
+    let end = Date.now()
+
+    let dectime = end-start + "ms"
+
+    con.query('insert into data values( "'+data.fileSize+'","'+data.algo+'","'+data.enctime+'","'+dectime+'")' )
+
+    socket.broadcast.emit('chat-message', data)
+  })
+
+  socket.on('feedback', (data) => {
+    socket.broadcast.emit('feedback', data)
+  })
+}
---
+++ public/main.js
@@ -0,0 +1,153 @@
+const socket = io()
+const clientsTotal = document.getElementById('client-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+const messageTone = new Audio('/message-tone.mp3')
+const key = ")(*&^%$#@!1234567890asdfghjklmnbvcxzqwertyuiop"
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  sendMessage()
+})
+
+socket.on('clients-total', (data) => { clientsTotal.innerText = `Total Clients: ${data}` })
+
+function sendMessage() {
+  if (messageInput.value === '') return
+  
+  //Encrypt
+  let start = Date.now()
+  const ciphertext = CryptoJS.AES.encrypt( messageInput.value, key, {
+    mode: CryptoJS.mode.CTR,
+    padding: CryptoJS.pad.Pkcs7,
+  }).toString();
+  let end = Date.now()
+  let algoUsed = "AES"
+
+  let file_bytes = messageInput.value.length;
+
+  let file_size = "";
+
+  if(file_bytes <= 1)
+  {
+    file_size = "Tiny (1 Byte)"
+  }
+  else if(file_bytes <= 25)
+  {
+    file_size = "Small (<=25 Bytes)"
+  }
+  else if(file_bytes <= 150)
+  {
+    file_size = "Medium (<=150 Bytes)"
+  }
+  else if(file_bytes <= 500)
+  {
+    file_size = "Large (<=500 Bytes)"
+  }
+  else file_size = "Huge (>500 Bytes)"
+
+  const data = {
+    name: nameInput.value,
+    message: ciphertext,
+    dateTime: new Date(),
+    fileSize: file_size,
+    algo: algoUsed,
+    enctime: end-start + "ms",
+  }
+  socket.emit('message', data)
+  addMessageToUI(true, data)
+  
+  messageInput.value = ''
+}
+
+socket.on('chat-message', (data) => {
+  messageTone.play()
+  addMessageToUI(false, data)
+})
+
+function addMessageToUI(isOwnMessage, data) {
+  clearFeedback()
+
+/*decrypting ciphertext using DES
+
+  const decrypted = CryptoJS.DES.decrypt(data.message, key, {
+    mode: CryptoJS.mode.CTR,
+    padding: CryptoJS.pad.Pkcs7,
+  }).toString(CryptoJS.enc.Utf8);
+*/
+
+/*decrypting ciphertext using TripleDES
+
+  const decrypted = CryptoJS.TripleDES.decrypt(data.message, key, {
+    mode: CryptoJS.mode.CTR,
+    padding: CryptoJS.pad.Pkcs7,
+  }).toString(CryptoJS.enc.Utf8);
+*/
+
+/*decrypting ciphertext using AES
+*/
+  const decrypted = CryptoJS.AES.decrypt(data.message, key, {
+    mode: CryptoJS.mode.CTR,
+    padding: CryptoJS.pad.Pkcs7,
+  }).toString(CryptoJS.enc.Utf8);
+  
+
+  /*decrypting ciphertext using RC4
+  
+  const decrypted = CryptoJS.RC4.decrypt(data.message, key, {
+    mode: CryptoJS.mode.CTR,
+    padding: CryptoJS.pad.Pkcs7,
+  }).toString(CryptoJS.enc.Utf8);
+*/
+  const element = `
+      <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
+          <p class="message">
+            ${decrypted}
+            <span>${data.name} ● ${moment(data.dateTime).fromNow()}</span>
+          </p>
+        </li>
+        `
+
+  messageContainer.innerHTML += element
+  scrollToBottom()
+}
+
+function scrollToBottom() {
+  messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
+
+messageInput.addEventListener('focus', (e) => {
+  socket.emit('feedback', {
+    feedback: `✍️ ${nameInput.value} is typing a message`,
+  })
+})
+
+messageInput.addEventListener('keypress', (e) => {
+  socket.emit('feedback', {
+    feedback: `✍️ ${nameInput.value} is typing a message`,
+  })
+})
+messageInput.addEventListener('blur', (e) => {
+  socket.emit('feedback', {
+    feedback: '',
+  })
+})
+
+socket.on('feedback', (data) => {
+  clearFeedback()
+  const element = `
+        <li class="message-feedback">
+          <p class="feedback" id="feedback">${data.feedback}</p>
+        </li>
+  `
+  messageContainer.innerHTML += element
+})
+
+function clearFeedback() {
+  document.querySelectorAll('li.message-feedback').forEach((element) => {
+    element.parentNode.removeChild(element)
+  })
+}
